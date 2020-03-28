219034e362ffb4eff3f2f793a7880356e0e57844
Paul McManus <pmcmanus2021@gmail.com>
2020-03-28 20:26:33 +0000

    make it better 2

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 function ProductItem(props) {
   return(
     <div>
-      <a href="/topsellers/product/:id">
+      <a href="/topsellers/product/:id(testtext)">
         <h1 className="product-link">{props.position}. {props.name} - £{props.price.toFixed(2)}</h1>
       </a>
     </div>
